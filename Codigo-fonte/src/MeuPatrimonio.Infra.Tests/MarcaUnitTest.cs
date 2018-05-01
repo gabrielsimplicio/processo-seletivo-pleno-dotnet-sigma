7d71f42838622d312676a3c1d713e9a1737abb71
Levrangeles da Silva Filho <levrangeles@gmail.com>
2018-05-01 11:49:45 -0400

    .

---
+++ Codigo-fonte/src/MeuPatrimonio.Infra.Tests/MarcaUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MeuPatrimonio.Infra.Data.ORM.EntityFramework.Contexts;
+using MeuPatrimonio.Infra.Data.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeuPatrimonio.Infra.Tests
+{
+    [TestClass]
+    public class MarcaUnitTest
+    {
+        [TestMethod]
+        public void DeveBuscarTodasAsMarcas()
+        {
+            var context = new MeuPatrimonioContext();
+            var repository = new MarcaRepository(context);
+            var marcas = repository.GetAll();
+
+            Assert.IsNotNull(marcas);
+        }
+    }
+}
