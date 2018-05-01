959ec56bc9af1369f2e28bcddb931e93c6ea38c6
Levrangeles da Silva Filho <levrangeles@gmail.com>
2018-05-01 17:59:15 -0400

    .

---
+++ Codigo-fonte/src/MeuPatrimonio.Application/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MeuPatrimonio.Domain.DTOs;
+using MeuPatrimonio.Domain.Entities;
+
+namespace MeuPatrimonio.Application.AutoMapper
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(mapper =>
+            {
+                mapper.CreateMap<MarcaDTO, Marca>();
+                mapper.CreateMap<Marca, MarcaDTO>();
+            });
+        }
+    }
+}
