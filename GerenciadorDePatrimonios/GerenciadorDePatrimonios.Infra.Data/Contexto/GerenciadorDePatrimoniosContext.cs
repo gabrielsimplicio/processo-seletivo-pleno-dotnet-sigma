11e63fd2811c65d54941f780bafca3d68e20c6c6
Jamil Tadeu Zazu <jamillzazu@gmail.com>
2018-05-04 07:27:48 -0400

    Database.SetInitializer

--- GerenciadorDePatrimonios/GerenciadorDePatrimonios.Infra.Data/Contexto/GerenciadorDePatrimoniosContext.cs
+++ GerenciadorDePatrimonios/GerenciadorDePatrimonios.Infra.Data/Contexto/GerenciadorDePatrimoniosContext.cs
@@ -10,7 +10,7 @@ namespace GerenciadorDePatrimonios.Infra.Data.Contexto
         public GerenciadorDePatrimoniosContext()
             : base("GerenciadorPatrimonioConnectionString")
         {
-
+            Database.SetInitializer(new GerenciadorPatrimonioDataContextInitializer());
         }
         public DbSet<Patrimonio> Patrimonios { get; set; }
         public DbSet<Marca> Marcas { get; set; }
@@ -40,7 +40,32 @@ namespace GerenciadorDePatrimonios.Infra.Data.Contexto
             modelBuilder.Configurations.Add(new PatrimonioConfiguration());
             modelBuilder.Configurations.Add(new MarcaConfiguration());
             modelBuilder.Configurations.Add(new ModeloConfiguration());
-            //base.OnModelCreating(modelBuilder);
+        }
+
+        public class GerenciadorPatrimonioDataContextInitializer : DropCreateDatabaseIfModelChanges<GerenciadorDePatrimoniosContext>
+        {
+            protected override void Seed(GerenciadorDePatrimoniosContext context)
+            {
+                context.Marcas.Add(new Marca { Nome = "Motorola" });
+                context.Marcas.Add(new Marca { Nome = "Samsung" });
+                context.Marcas.Add(new Marca { Nome = "Apple" });
+                context.SaveChanges();
+
+                context.Modelos.Add(new Modelo { Nome = "Moto X", MarcaId = 1 });
+                context.Modelos.Add(new Modelo { Nome = "Moto X2", MarcaId = 1 });
+                context.Modelos.Add(new Modelo { Nome = "J7", MarcaId = 2 });
+                context.Modelos.Add(new Modelo { Nome = "J5", MarcaId = 2 });
+                context.Modelos.Add(new Modelo { Nome = "7 PLUS", MarcaId = 3 });
+                context.Modelos.Add(new Modelo { Nome = "X", MarcaId = 3 });
+                context.SaveChanges();
+
+                context.Patrimonios.Add(new Patrimonio { Nome = "Celular da Empresa", MarcaId = 1, ModeloId = 1, Descricao = " ", NumeroTombo = new System.Guid() });
+                context.Patrimonios.Add(new Patrimonio { Nome = "Celular Pessoal", MarcaId = 2, ModeloId = 2, Descricao = " ", NumeroTombo = new System.Guid() });
+                context.Patrimonios.Add(new Patrimonio { Nome = "Celular do Escritorio", MarcaId = 3, ModeloId = 3, Descricao = " ", NumeroTombo = new System.Guid() });
+                context.SaveChanges();
+
+                base.Seed(context);
+            }
         }
     }
 }
